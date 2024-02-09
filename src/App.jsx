d692bd0484a3de02f081ae09c21bb90234ec7568
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2024-02-09 13:28:19 -0300

    estructura

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const ApiKey = "f64cd05f225040e5b1636b12d1f92775";
+
+  return <></>;
+}
+
+export default App;
